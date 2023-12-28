e526409d5555f632b2fe5885a0b5f5a30dcb3d54
Tom Lienard <tom.lienrd@gmail.com>
2023-12-28 14:23:36 +0100

    fix: batch of small fixes

--- apps/dashboard/src/components/OgSplash.tsx
+++ apps/dashboard/src/components/OgSplash.tsx
@@ -14,16 +14,18 @@ import { OgImage } from "./OgImage";
 import { ArrowRightIcon } from "./icons/ArrowRightIcon";
 import { CustomLink } from "./CustomLink";
 import { ArrowLeftIcon } from "./icons/ArrowLeftIcon";
+import { CopyIcon } from "./icons/CopyIcon";
 
 interface OgImageWrapperProps {
   href?: string
   onClick?: (event: MouseEvent<HTMLElement>) => void
   elements?: OGElement[]
   children?: ReactNode
+  copiable?: (event: MouseEvent<HTMLSpanElement>) => void
   deletable?: (event: MouseEvent<HTMLSpanElement>) => void
 }
 
-function OgImageWrapper({ href, onClick, elements, children, deletable }: OgImageWrapperProps) {
+function OgImageWrapper({ href, onClick, elements, children, copiable, deletable }: OgImageWrapperProps) {
   const Tag = href ? Link : 'button'
 
   return (
@@ -34,8 +36,13 @@ function OgImageWrapper({ href, onClick, elements, children, deletable }: OgImag
         </Suspense>
       ) : null}
       {children}
+      {copiable ? (
+        <button className="absolute right-8 top-1 p-1 bg-black/60 rounded text-gray-300 hover:text-gray-200 hidden group-hover:block" onClick={copiable} type="button">
+          <CopyIcon />
+        </button>
+      ) : null}
       {deletable ? (
-        <button className="absolute right-0 top-0 p-2 text-gray-600 hover:text-gray-900 hidden group-hover:block" onClick={deletable} type="button">
+        <button className="absolute right-1 top-1 p-1 bg-black/60 rounded text-gray-300 hover:text-gray-200 hidden group-hover:block" onClick={deletable} type="button">
           <DeleteIcon />
         </button>
       ) : null}
@@ -73,23 +80,39 @@ export function OgSplash({ route }: OgSplashProps) {
     setOgImages(images)
   }, [image])
 
-  function copyTemplate(template: Template) {
+  function copyTemplate(template: Template, push = true) {
     return function onClick() {
       const id = createElementId()
       const key = `og-${id}`
 
       localStorage.setItem(key, JSON.stringify(template.elements))
-      router.push(`/?i=${id}`)
+
+      if (push) {
+        router.push(`/?i=${id}`)
+      } else {
+        setOgImages([{ id: key, content: template.elements }, ...ogImages])
+      }
     }
   }
 
-  function deleteOgImage(ogImage: string) {
+  function copyOgImage(ogImage: OGImage) {
+    const copy = copyTemplate({ name: ogImage.id, elements: ogImage.content }, false)
+
     return function onClick(event: MouseEvent<HTMLSpanElement>) {
       event.preventDefault();
       event.stopPropagation();
 
-      localStorage.removeItem(ogImage)
-      setOgImages(ogImages.filter(({ id }) => id !== ogImage))
+      copy()
+    }
+  }
+
+  function deleteOgImage(ogImage: OGImage) {
+    return function onClick(event: MouseEvent<HTMLSpanElement>) {
+      event.preventDefault();
+      event.stopPropagation();
+
+      localStorage.removeItem(ogImage.id)
+      setOgImages(ogImages.filter(({ id }) => id !== ogImage.id))
     }
   }
 
@@ -97,12 +120,19 @@ export function OgSplash({ route }: OgSplashProps) {
     <>
       <OgEditor height={630} initialElements={INITIAL_ELEMENTS} localStorageKey={image ?? 'splash'} width={1200} />
       {image ? null : (
-        <div className="w-screen h-screen bg-black/20 flex justify-center items-center absolute top-0 left-0 z-10 backdrop-blur-[1px]">
-          <div className="p-8 rounded-md bg-white shadow-lg shadow-gray-300 w-[calc((300px*3)+(2rem*2)+(0.5rem*2))]">
+        <div className="w-screen h-screen bg-black/10 flex justify-center items-center absolute top-0 left-0 z-10 backdrop-blur-[1px]">
+          <div className="p-8 rounded-md bg-white shadow-lg shadow-gray-200 w-[980px] h-[636px]">
+            <div className="flex items-center justify-between">
+              <h1 className="text-gray-900 text-2xl">OG Studio</h1>
+              <CustomLink href="/" icon={<div className="w-6 h-6 rounded-full bg-gray-200 animate-pulse" />} iconPosition="right">
+                Guest
+              </CustomLink>
+            </div>
+            <div className="h-[1px] w-full bg-gray-100 my-8" />
             {route === 'splash' ? (
               <>
                 <div className="flex flex-col gap-4">
-                  <div className="flex flex-row justify-between items-center">
+                  <div className="flex justify-between items-center">
                     <h2 className="text-gray-800 text-xl">Templates</h2>
                     <CustomLink href="/templates" icon={<ArrowRightIcon />} iconPosition="right">
                       See all
@@ -116,7 +146,7 @@ export function OgSplash({ route }: OgSplashProps) {
                 </div>
                 <div className="h-[1px] w-full bg-gray-100 my-8" />
                 <div className="flex flex-col gap-4">
-                  <div className="flex flex-row justify-between items-center">
+                  <div className="flex justify-between items-center">
                     <h2 className="text-gray-800 text-xl">My OG images</h2>
                     <CustomLink href="/my-images" icon={<ArrowRightIcon />} iconPosition="right">
                       See all
@@ -127,14 +157,20 @@ export function OgSplash({ route }: OgSplashProps) {
                       <AddIcon height="1.4em" width="1.4em" /> Start from scratch
                     </OgImageWrapper>
                     {ogImages.slice(0, 2).map(ogImage => (
-                      <OgImageWrapper deletable={deleteOgImage(ogImage.id)} elements={ogImage.content} href={`/?i=${ogImage.id.replace('og-', '')}`} key={ogImage.id} />
+                      <OgImageWrapper
+                        copiable={copyOgImage(ogImage)}
+                        deletable={deleteOgImage(ogImage)}
+                        elements={ogImage.content}
+                        href={`/?i=${ogImage.id.replace('og-', '')}`}
+                        key={ogImage.id}
+                      />
                     ))}
                   </div>
                 </div>
               </>
             ) : route === 'templates' ? (
               <div className="flex flex-col gap-4">
-                <div className="flex flex-row justify-between items-center">
+                <div className="flex justify-between items-center">
                   <h2 className="text-gray-800 text-xl">All templates</h2>
                   <CustomLink href="/" icon={<ArrowLeftIcon />}>
                     Back
@@ -148,7 +184,7 @@ export function OgSplash({ route }: OgSplashProps) {
               </div>
             ) : (
               <div className="flex flex-col gap-4">
-                <div className="flex flex-row justify-between items-center">
+                <div className="flex justify-between items-center">
                   <h2 className="text-gray-800 text-xl">My OG images</h2>
                   <CustomLink href="/" icon={<ArrowLeftIcon />}>
                     Back
@@ -159,7 +195,13 @@ export function OgSplash({ route }: OgSplashProps) {
                     <AddIcon height="1.4em" width="1.4em" /> Start from scratch
                   </OgImageWrapper>
                   {ogImages.map(ogImage => (
-                    <OgImageWrapper deletable={deleteOgImage(ogImage.id)} elements={ogImage.content} href={`/?i=${ogImage.id.replace('og-', '')}`} key={ogImage.id} />
+                    <OgImageWrapper
+                      copiable={copyOgImage(ogImage)}
+                      deletable={deleteOgImage(ogImage)}
+                      elements={ogImage.content}
+                      href={`/?i=${ogImage.id.replace('og-', '')}`}
+                      key={ogImage.id}
+                    />
                   ))}
                 </div>
               </div>
