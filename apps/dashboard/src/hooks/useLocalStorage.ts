b5527d195a224b9a90fd49d311f9019d857c830f
Tom Lienard <tom.lienrd@gmail.com>
2023-12-29 13:02:05 +0100

    fix: zoom bugs

---
+++ apps/dashboard/src/hooks/useLocalStorage.ts
@@ -0,0 +1,21 @@
+import { useEffect, useMemo, useState } from "react";
+
+export function useLocalStorage<T>(initialValue: T, key: string) {
+  const [value, setValue] = useState(initialValue)
+
+  useEffect(() => {
+    const item = localStorage.getItem(key)
+
+    if (item) {
+      setValue(JSON.parse(item) as T)
+    }
+  }, [key])
+
+  return useMemo(() => [
+    value,
+    (newValue: T) => {
+      setValue(newValue)
+      localStorage.setItem(key, JSON.stringify(newValue))
+    }
+  ], [key, value])
+}
