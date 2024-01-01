9e527ff091633b04c304a9b1a58356e54fc79a1a
Tom Lienard <tom.lienrd@gmail.com>
2024-01-01 09:28:45 +0100

    feat: local libsql-server

--- apps/dashboard/drizzle.config.ts
+++ apps/dashboard/drizzle.config.ts
@@ -1,12 +1,12 @@
-import 'dotenv/config';
-import type { Config } from 'drizzle-kit';
+import "dotenv/config";
+import type { Config } from "drizzle-kit";
 
 export default {
-  schema: './src/lib/db/schema.ts',
-  out: './drizzle',
-  driver: 'turso',
+  schema: "./src/lib/db/schema.ts",
+  out: "./drizzle",
+  driver: "turso",
   dbCredentials: {
-    url: process.env.DATABASE_URL ?? '',
+    url: process.env.DATABASE_URL ?? "",
     authToken: process.env.DATABASE_AUTH_TOKEN,
   },
 } satisfies Config;
--- apps/dashboard/drizzle/migrate.ts
+++ apps/dashboard/drizzle/migrate.ts
@@ -1,13 +1,12 @@
-import 'dotenv/config';
-import { migrate } from 'drizzle-orm/libsql/migrator';
-import { client, db } from '../src/lib/db/db';
+import "dotenv/config";
+import { migrate } from "drizzle-orm/libsql/migrator";
+import { client, db } from "../src/lib/db/db";
 
 async function run() {
   // This will run migrations on the database, skipping the ones already applied
-  await migrate(db, { migrationsFolder: './drizzle' });
+  await migrate(db, { migrationsFolder: "./drizzle" });
   // Don't forget to close the connection, otherwise the script will hang
   client.close();
 }
 
-run().then(null).catch(console.error)
-
+run().then(null).catch(console.error);
