6a63cda3c909825592d866f11444b87f7f61094a
Tom Lienard <tom.lienrd@gmail.com>
2024-01-21 11:11:05 +0100

    feat: add sitemap

---
+++ apps/dashboard/src/app/sitemap.ts
@@ -0,0 +1,45 @@
+import type { MetadataRoute } from "next";
+import { TEMPLATES } from "../lib/templates";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://ogstudio.app",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://ogstudio.app/templates",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    ...TEMPLATES.map<MetadataRoute.Sitemap[number]>((template) => ({
+      url: `https://ogstudio.app/templates/${encodeURIComponent(
+        template.name.toLowerCase(),
+      )}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    })),
+    {
+      url: "https://ogstudio.app/my-images",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://ogstudio.app/profile",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+    {
+      url: "https://ogstudio.app/login",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
