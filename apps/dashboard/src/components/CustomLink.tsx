b6d6406b0f5d0785c940f90256aa61e2832abc9c
Tom Lienard <tom.lienrd@gmail.com>
2024-03-23 14:09:04 +0000

    fix: links

--- apps/dashboard/src/components/CustomLink.tsx
+++ apps/dashboard/src/components/CustomLink.tsx
@@ -1,4 +1,4 @@
-import type Link from "next/link";
+import Link from "next/link";
 import type { ComponentProps, ReactNode } from "react";
 
 interface CustomLinkProps {
@@ -18,12 +18,12 @@ export function CustomLink({
   target,
   children,
 }: CustomLinkProps) {
-  const Tag = href ? "a" : "button";
+  const Tag = href ? Link : "button";
 
   return (
     <Tag
       className="flex gap-2 items-center px-3 py-1 rounded-full text-gray-600 hover:text-gray-900 hover:bg-gray-50 select-none"
-      href={href}
+      href={href ?? ""}
       onClick={onClick}
       target={target}
     >
