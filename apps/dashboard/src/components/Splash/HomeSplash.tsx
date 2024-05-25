b35cff641489cbf02740e57aa46c2373335ae6ab
Tom Lienard <tom.lienrd@gmail.com>
2024-05-25 08:50:52 +0100

    feat: add 404 pages and update templates slugs (#92)

--- apps/dashboard/src/components/Splash/HomeSplash.tsx
+++ apps/dashboard/src/components/Splash/HomeSplash.tsx
@@ -1,7 +1,7 @@
 import { Button, Flex, Separator, Text } from "@radix-ui/themes";
 import Link from "next/link";
 import { ArrowRightIcon } from "../icons/ArrowRightIcon";
-import { TEMPLATES } from "../../lib/templates";
+import { TEMPLATES, toTemplateSlug } from "../../lib/templates";
 import { OgImage } from "../OgImage";
 import { HomeSplashMyImages } from "./HomeSplashMyImages";
 
@@ -22,7 +22,7 @@ export function HomeSplash() {
           {TEMPLATES.slice(0, 3).map((template) => (
             <OgImage
               elements={template.elements}
-              href={`/templates/${template.name.toLowerCase()}`}
+              href={`/templates/${toTemplateSlug(template)}`}
               key={template.name}
               mockDynamicTexts
               name={template.name}
