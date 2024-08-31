3f179a8fe830b8e4f9f60e44bee52accdb7cfa78
Tom Lienard <tom.lienrd@gmail.com>
2024-08-31 11:31:41 +0100

    fix: update deps

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -11,6 +11,7 @@ export default {
   schema: "./schema.ts",
   out: "./drizzle",
   driver: "turso",
+  dialect: "sqlite",
   dbCredentials: {
     url: process.env.DATABASE_URL ?? "",
     authToken: process.env.DATABASE_AUTH_TOKEN,
