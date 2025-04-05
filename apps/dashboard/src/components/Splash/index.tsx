df5cb29a94731464a83a2d71de8d861566770057
Tom Lienard <tom.lienrd@gmail.com>
2025-04-05 15:12:59 +0100

    add responsive menu

--- apps/dashboard/src/components/Splash/index.tsx
+++ apps/dashboard/src/components/Splash/index.tsx
@@ -2,20 +2,53 @@
 import type { ReactNode } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { Avatar, Box, Button, Flex, Text } from "@radix-ui/themes";
+import {
+  Avatar,
+  Box,
+  Button,
+  DropdownMenu,
+  Flex,
+  Text,
+} from "@radix-ui/themes";
 import { usePathname } from "next/navigation";
 import { OgEditor } from "../OgEditor";
 import { GitHubIcon } from "../icons/GitHubIcon";
 import { useUser } from "../../lib/hooks/useUser";
 import { useIsSignedIn } from "../../lib/hooks/useIsSignedIn";
+import { MenuIcon } from "../icons/MenuIcon";
+
+function ProfileButton() {
+  const { data } = useUser();
+  const isSignedIn = useIsSignedIn();
+  const pathname = usePathname();
+
+  return (
+    <Button
+      asChild
+      color="gray"
+      radius="full"
+      variant="ghost"
+      highContrast={pathname === "/profile" || pathname === "/login"}
+    >
+      <Link href={isSignedIn ? "/profile" : "/login"}>
+        <Avatar
+          fallback="G"
+          radius="full"
+          size="1"
+          src={isSignedIn ? data?.session.user?.avatar : undefined}
+          ml="-1"
+        />
+        {isSignedIn ? data?.session.user?.name : "Guest"}
+      </Link>
+    </Button>
+  );
+}
 
 interface OgSplashProps {
   children: ReactNode;
 }
 
 export function Splash({ children }: OgSplashProps) {
-  const { data } = useUser();
-  const isSignedIn = useIsSignedIn();
   const pathname = usePathname();
 
   return (
@@ -46,11 +79,7 @@ export function Splash({ children }: OgSplashProps) {
             borderRadius: "var(--radius-5)",
           }}
         >
-          <Flex
-            justify="between"
-            mt="-3"
-            className="sm:items-center items-start"
-          >
+          <Flex justify="between" align="center" mt="-3">
             <Flex align="center" gap="6">
               <Text asChild size="5" className="font-[Raleway]" weight="medium">
                 <Link className="flex gap-2 items-center min-w-fit" href="/">
@@ -69,6 +98,7 @@ export function Splash({ children }: OgSplashProps) {
                 radius="full"
                 variant="ghost"
                 highContrast={pathname.startsWith("/templates")}
+                className="hidden sm:inline-flex"
               >
                 <Link href="/templates">Templates</Link>
               </Button>
@@ -78,45 +108,51 @@ export function Splash({ children }: OgSplashProps) {
                 radius="full"
                 variant="ghost"
                 highContrast={pathname === "/tools/open-graph-image-checker"}
+                className="hidden sm:inline-flex"
               >
                 <Link href="/tools/open-graph-image-checker">
                   Open Graph Checker
                 </Link>
               </Button>
             </Flex>
-            <Flex align="center" gap="6">
+            <Flex align="center" gap="6" className="hidden sm:flex">
               <Button asChild color="gray" radius="full" variant="ghost">
                 <Link href="https://github.com/QuiiBz/ogstudio" target="_blank">
                   <GitHubIcon />
                   GitHub
                 </Link>
               </Button>
-              <Button
-                asChild
-                color="gray"
-                radius="full"
-                variant="ghost"
-                highContrast={pathname === "/profile" || pathname === "/login"}
-              >
-                <Link href={isSignedIn ? "/profile" : "/login"}>
-                  <Avatar
-                    fallback="G"
-                    radius="full"
-                    size="1"
-                    src={isSignedIn ? data?.session.user?.avatar : undefined}
-                    ml="-1"
-                  />
-                  {isSignedIn ? data?.session.user?.name : "Guest"}
-                </Link>
-              </Button>
+              <ProfileButton />
             </Flex>
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger className="sm:hidden">
+                <MenuIcon />
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content variant="soft" className="sm:hidden">
+                <DropdownMenu.Item asChild>
+                  <Link href="/templates">Templates</Link>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item asChild>
+                  <Link href="/tools/open-graph-image-checker">
+                    Open Graph Checker
+                  </Link>
+                </DropdownMenu.Item>
+                <DropdownMenu.Item asChild>
+                  <Link
+                    href="https://github.com/QuiiBz/ogstudio"
+                    target="_blank"
+                  >
+                    <GitHubIcon />
+                    GitHub
+                  </Link>
+                </DropdownMenu.Item>
+                <DropdownMenu.Separator />
+                <DropdownMenu.Item>
+                  <ProfileButton />
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
           </Flex>
-          {/* <Text as="p" className="lg:w-2/3" size="2"> */}
-          {/*   Create static or dynamic OG (Open Graph) images with an intuitive, */}
-          {/*   Figma-like visual editor. Browse ready-to-use templates, and export */}
-          {/*   your images to SVG/PNG or to a dynamic URL. */}
-          {/* </Text> */}
-          {/* <Separator className="opacity-50" my="6" size="4" /> */}
           {children}
         </Box>
       </Flex>
---
+++ apps/dashboard/src/components/icons/MenuIcon.tsx
@@ -0,0 +1,19 @@
+import { type SVGProps } from "react";
+
+export function MenuIcon(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}
+    >
+      {/* Icon from Material Symbols Light by Google - https://github.com/google/material-design-icons/blob/master/LICENSE */}
+      <path
+        fill="currentColor"
+        d="M4.5 17.27q-.213 0-.356-.145T4 16.768t.144-.356t.356-.143h15q.213 0 .356.144q.144.144.144.357t-.144.356t-.356.143zm0-4.77q-.213 0-.356-.144T4 11.999t.144-.356t.356-.143h15q.213 0 .356.144t.144.357t-.144.356t-.356.143zm0-4.77q-.213 0-.356-.143Q4 7.443 4 7.23t.144-.356t.356-.143h15q.213 0 .356.144T20 7.23t-.144.356t-.356.144z"
+      />
+    </svg>
+  );
+}
